7b3ae450d8ab9cbbc342223014e0e2161e72efe3
Thành Nguyễn Trí <91453590+thahn0402@users.noreply.github.com>
2024-01-01 15:19:17 +0700

    update forbidden

--- src/main/java/control/ManagerAccountControl.java
+++ src/main/java/control/ManagerAccountControl.java
@@ -14,26 +14,36 @@ import java.util.List;
 public class ManagerAccountControl extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-        String indexPage = request.getParameter("index");
-        if (indexPage == null) {
-            indexPage = "1";
-        }
-        int index = Integer.parseInt(indexPage);
         HttpSession session = request.getSession();
         Account a = (Account) session.getAttribute("acc");
-        int id = a.getId();
-        DAO dao = new DAO();
-        int count = dao.getTotalAccount();
-        int endPage = count / 9;
-        if (count % 9 != 0) {
-            endPage++;
-        }
-        List<Account> list = dao.getAllAccount(index);
 
-        request.setAttribute("endP", endPage);
-        request.setAttribute("listP", list);
-        request.setAttribute("tag", index);
-        request.getRequestDispatcher("manageraccount.jsp").forward(request, response);
+        // Check if the user is an admin
+        if (a != null && a.getIsAdmin() == 1) {
+
+            String indexPage = request.getParameter("index");
+            if (indexPage == null) {
+                indexPage = "1";
+            }
+            int index = Integer.parseInt(indexPage);
+//        HttpSession session = request.getSession();
+//        Account a = (Account) session.getAttribute("acc");
+            int id = a.getId();
+            DAO dao = new DAO();
+            int count = dao.getTotalAccount();
+            int endPage = count / 9;
+            if (count % 9 != 0) {
+                endPage++;
+            }
+            List<Account> list = dao.getAllAccount(index);
+
+            request.setAttribute("endP", endPage);
+            request.setAttribute("listP", list);
+            request.setAttribute("tag", index);
+            request.getRequestDispatcher("manageraccount.jsp").forward(request, response);
+        } else {
+            // If the user is not an admin, redirect or handle accordingly
+            response.sendRedirect("forbidden.jsp");
+        }
     }
 
     @Override
--- src/main/java/control/ManagerProductControl.java
+++ src/main/java/control/ManagerProductControl.java
@@ -14,26 +14,35 @@ import java.util.List;
 public class ManagerProductControl extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-        String indexPage = request.getParameter("index");
-        if (indexPage == null) {
-            indexPage = "1";
-        }
-        int index = Integer.parseInt(indexPage);
         HttpSession session = request.getSession();
         Account a = (Account) session.getAttribute("acc");
-        int id = a.getId();
-        DAO dao = new DAO();
-        int count = dao.getTotalProductBySellId(id);
-        int endPage = count / 9;
-        if (count % 9 != 0) {
-            endPage++;
-        }
-        List<Product> list = dao.getProductBySellID(id, index);
 
-        request.setAttribute("endP", endPage);
-        request.setAttribute("listP", list);
-        request.setAttribute("tag", index);
-        request.getRequestDispatcher("managerproduct.jsp").forward(request, response);
+        // Check if the user is an admin
+        if (a != null && a.getIsSell() == 1) {
+            String indexPage = request.getParameter("index");
+            if (indexPage == null) {
+                indexPage = "1";
+            }
+            int index = Integer.parseInt(indexPage);
+//        HttpSession session = request.getSession();
+//        Account a = (Account) session.getAttribute("acc");
+            int id = a.getId();
+            DAO dao = new DAO();
+            int count = dao.getTotalProductBySellId(id);
+            int endPage = count / 9;
+            if (count % 9 != 0) {
+                endPage++;
+            }
+            List<Product> list = dao.getProductBySellID(id, index);
+
+            request.setAttribute("endP", endPage);
+            request.setAttribute("listP", list);
+            request.setAttribute("tag", index);
+            request.getRequestDispatcher("managerproduct.jsp").forward(request, response);
+        } else {
+            // If the user is not an admin, redirect or handle accordingly
+            response.sendRedirect("forbidden.jsp");
+        }
     }
 
     @Override
